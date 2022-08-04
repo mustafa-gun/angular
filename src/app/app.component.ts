038aa7bcc952581c17b06566c893473bbec51195
mustafa-gun <mustafagun.gs@gmail.com>
2022-08-04 10:36:23 +0300

    Add project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { animate, style, trigger, state, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  animations: [
+    trigger('toggleClick', [
+      state('true', style({
+        left: '100px'
+      })),
+      state ('false', style({
+        left: '0px'
+      })),
+      transition('true => false', animate('500ms linear')),
+      transition('false => true', animate('500ms linear')),
+    ])
+  ],
+  styles: ['div {height: 100px; width: 100px; position: relative;}']
 })
 export class AppComponent {
   title = 'angular';
+  public margin:number = 0;
+  public bgColor:string = "red";
+  public isClick: string = 'false';
+  toggleIsCorrect() {
+    this.isClick = this.isClick === 'true' ? 'false' : 'true';
+  }
+  click() {
+    animate("5s 10ms cubic-bezier(.17,.67,.88,.1)")
+  }
+  mOver() {
+    this.bgColor = "blueviolet";
+  }
+  mLeave() {
+    this.bgColor = "blue"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_BASE_HREF, LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers:
+  [
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
